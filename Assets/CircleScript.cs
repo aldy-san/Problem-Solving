d2583a5451e4ff9cab0dc245d8ad1c529656b6cc
aldy-san <nsmaulidi12@gmail.com>
2021-12-22 17:48:05 +0700

    Movement With Arrow

--- Assets/CircleScript.cs
+++ Assets/CircleScript.cs
@@ -6,26 +6,15 @@ public class CircleScript : MonoBehaviour
 {
     public Rigidbody2D rb;
     public float speed;
-    public float xInitialForce;
-    public float yInitialForce;
-    // Start is called before the first frame update
-    void Start()
-    {
-        float yRandomInitialForce = Random.Range(-yInitialForce, yInitialForce);
-        float randomDirection = Random.Range(0,2);
-
-        if (randomDirection < 1.0f)
-        {
-            rb.AddForce(new Vector2(-xInitialForce, yRandomInitialForce) * speed * 5);
-        } else
-        {
-            rb.AddForce(new Vector2(xInitialForce, yRandomInitialForce) * speed * 5);
-        }
-    }
-
-    // Update is called once per frame
+    Vector2 movement;
     void Update()
     {
-        
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
     }
 }
